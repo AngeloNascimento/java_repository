4ead75c6d7cd735170989a430375fe4cb9a4de3a
AngeloNascimento <81114877+AngeloNascimento@users.noreply.github.com>
2024-09-14 09:35:58 -0300

    Add files via upload

---
+++ ArrayList/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+/*
+* Diferente dos arrays tradicionais, o ArrayList pode aumentar ou diminuir de tamanho
+* automaticamente conforme elementos são adicionados ou removidos
+* ele oferece metodos uteis para manipular a lista de elementos
+* como adicionar, remover, buscar e iterar sobre os elementos.
+*
+* */
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        //List
+        //adicionar elementos- usa-se o metodo add();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+
+        //verificar o tamanho do Array- usa-se o metodo size();
+        //ex
+        int tamanho = numeros.size();
+        System.out.println(numeros);
+
+        //recuperar ou acessar- usa-se o metodo get();
+        //ex
+        int primeiro = numeros.get(0);//vai acessar o primeiro elemento do array
+        int ultimo = numeros.get(tamanho - 1);//vai acessar o ultimo elemento do array
+
+        //remover elementos- usa-se o metodo remove()
+        //ex
+        numeros.remove(0);//na remoção pode-se remover o elemento através do indice ou pelo proprio elemento
+
+
+        //exibir os elementos do arraylist
+        //ex
+        for(Integer numero: numeros ){//pode ser percorrida por iteração
+            System.out.println(numero);
+        }
+
+       /* for (int i=0; i<numeros.size(); i++){
+            System.out.println(numeros.get(i));
+        }*/
+
+
+
+
+
+    }
+}
