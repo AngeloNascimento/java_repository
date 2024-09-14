aef9a1456c30997d8150215018dc32ecfdff2a93
AngeloNascimento <81114877+AngeloNascimento@users.noreply.github.com>
2024-09-14 08:47:16 -0300

    Add files via upload

---
+++ ex07_repeticao_for/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+       /* for (int cc = 0; cc <= 3; cc++){
+            System.out.println("cambalhota");//variável; controle para finalizar laço; soma mais 1 a cada repetição
+        }
+        */
+       /* for(int cc= 2; cc<=100;  cc+=2){
+            System.out.println(cc);
+
+    }*/
+        for(int i = 1; i <= 3; i++){
+            for (int j = 0; j <=2; j+=2){
+                System.out.println("i = " + i+ ",J= " + j);
+            }
+
+        }
+
+
+
+
+        /*o codigo a cima é a mesma coisa que:
+        * int cc = 0;
+        * while(cc<=4){
+        *   System.out.println("cambalhota");
+        *   cc++;
+        * }
+        * */
+    }
+}
