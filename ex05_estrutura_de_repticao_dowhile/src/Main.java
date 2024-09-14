99350a5933eacfa735e2c4f0dcfc955bf27d5ce8
AngeloNascimento <81114877+AngeloNascimento@users.noreply.github.com>
2024-09-14 08:46:52 -0300

    Add files via upload

---
+++ ex05_estrutura_de_repticao_dowhile/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cc = 0;
+        do{//do é igual a "faça"
+            System.out.println("cambalhota " + (cc+1));
+            cc++;
+        }while (cc < 4);//teste logico do "do-while" é feito no final.
+    }
+}
