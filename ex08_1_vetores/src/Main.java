5117774a44f589f2b5a03ba3f986001720c4902a
AngeloNascimento <81114877+AngeloNascimento@users.noreply.github.com>
2024-09-14 08:47:33 -0300

    Add files via upload

---
+++ ex08_1_vetores/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+       /* int n[] = {3,4,2,6,5,9}; //vetor de inteiros
+        System.out.println("O total de casas de N " + n.length);
+        for(int c = 0; c<=5; c++){
+            System.out.println("na posição " + c +" temos " +n[c]);
+        }*/
+
+       /* String mes[] = {"jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"};
+        String tot[] = {"31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"};
+
+        for(int i = 0; i <mes.length; i++){
+            System.out.println("o mes " + mes[i] + " tem " + tot[i] + " dias");
+        }*/
+
+        /*double v[] = {3.5, 2.75, 9, -4.5};
+        Arrays.sort(v);//ordena os valores do vetor
+        for (double valor: v){//for-it funciona exclusivamento em vetores
+            System.out.println(valor);
+        }*/
+
+        /*//fazendo busca em vetores
+        double v[] = {3.5, 2.75, 9, -4.5};
+
+        int pos = Arrays.binarySearch(v, 9);//binarySearch faz a busca binaria
+        System.out.println(pos);*/
+
+        
+
+
+
+
+
+
+
+
+    }
+
+}
