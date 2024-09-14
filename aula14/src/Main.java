d2a2a7988c02b7f7d86443eace4a25a9412e509e
AngeloNascimento <81114877+AngeloNascimento@users.noreply.github.com>
2024-09-14 08:44:25 -0300

    Add files via upload

---
+++ aula14/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+       /* //video
+        Video v[] = new Video[3];
+        v[0] = new Video("transformes");
+        v[1] = new Video("Kong");
+        v[2] = new Video("Godzilla");
+
+        //gafanhoto
+        Gafanhoto g[] = new Gafanhoto[2];
+
+        g[0] = new Gafanhoto("TEste", 22, "M", "Tet");*/
+
+
+       /* System.out.println(g[0].toString());
+        System.out.println(v[0].toString());*/
+
+
+       /* //filme1
+        Vizualizacao vz[] = new Vizualizacao[5];
+        vz[0] = new Vizualizacao(g[0], v[0]);
+        vz[0].avaliar();
+        System.out.println(vz[0].toString());*/
+
+
+        /*//filme2
+        vz[1] = new Vizualizacao(g[0], v[1]);
+        vz[1].avaliar(90.1f);
+        System.out.println(vz[1].toString());//parou aqui*/
+
+
+    }
+}
